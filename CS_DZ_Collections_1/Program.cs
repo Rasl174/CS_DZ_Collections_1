96a148ddebad712ceb88189496d4a0c05c3c9c06
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-25 13:43:52 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_Collections_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Collections_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> carsBrandsCountries = new Dictionary<string, string>();
+
+            carsBrandsCountries.Add("Ауди", "Германия");
+            carsBrandsCountries.Add("Лада", "Россия");
+
+            string userInput;
+            bool canExit = false;
+
+            while (canExit == false)
+            {
+                Console.Write("Введите марку авто или введите exit для выхода: ");
+                userInput = Console.ReadLine();
+
+                if (carsBrandsCountries.ContainsKey(userInput))
+                    Console.WriteLine(carsBrandsCountries[userInput]);
+                else
+                    Console.WriteLine("Этого нет в словаре");
+
+                if (userInput == "exit")
+                    canExit = true;
+            }
+        }
+    }
+}
